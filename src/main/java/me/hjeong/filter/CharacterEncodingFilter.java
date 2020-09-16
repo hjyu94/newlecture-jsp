81f13ebd2d4c1881a2905d0f36be44e12e2a85e5
효정 <hjeong.you@gmail.com>
2020-09-16 23:43:16 +0900

    서블릿 필터 매핑 - 어노테이션

--- src/main/java/me/hjeong/filter/CharacterEncodingFilter.java
+++ src/main/java/me/hjeong/filter/CharacterEncodingFilter.java
@@ -1,8 +1,10 @@
 package me.hjeong.filter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
+@WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
 
     @Override
@@ -11,9 +13,8 @@ public class CharacterEncodingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("before filter");
+        servletRequest.setCharacterEncoding("UTF-8");
         filterChain.doFilter(servletRequest, servletResponse);
-        System.out.println("after filter");
     }
 
     @Override
