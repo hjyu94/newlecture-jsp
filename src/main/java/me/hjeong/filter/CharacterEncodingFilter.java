0c7268d6f41c607329e18bfd5fa8247c51a13f7b
효정 <hjeong.you@gmail.com>
2020-09-16 23:40:06 +0900

    서블릿 필터 매핑 - web.xml

---
+++ src/main/java/me/hjeong/filter/CharacterEncodingFilter.java
@@ -0,0 +1,23 @@
+package me.hjeong.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CharacterEncodingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("before filter");
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("after filter");
+    }
+
+    @Override
+    public void destroy() {
+    }
+    
+}
--- src/main/java/me/hjeong/web/NoticeReg.java
+++ src/main/java/me/hjeong/web/NoticeReg.java
@@ -21,7 +21,7 @@ public class NoticeReg extends HttpServlet {
             톰캣의 기본 방식은 사용자가 입력한 값을 ISO-8859-1 방식으로 디코딩함.
             사용자가 입력한 값을 UTF-8 로 받아볼 것임을 설정해줘야 한다.
          */
-        req.setCharacterEncoding("UTF-8");
+//        req.setCharacterEncoding("UTF-8");
 
         PrintWriter out = resp.getWriter();
 
