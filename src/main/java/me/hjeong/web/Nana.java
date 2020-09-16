c54ea980f3aff4c26cddc3ae82805323ba4f2d06
효정 <hjeong.you@gmail.com>
2020-09-16 23:10:06 +0900

    한글 깨지지 않도록 하기 (UTF-8 인코딩)

--- src/main/java/me/hjeong/web/Nana.java
+++ src/main/java/me/hjeong/web/Nana.java
@@ -10,9 +10,19 @@ import java.io.PrintWriter;
 
 @WebServlet("/hi")
 public class Nana extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*
+            한글이 깨지는 두 가지 이유
+            (1) 서버에서 한글을 지원하지 않는 문자코드로 인코딩한 경우
+            (2) 서버에서는 UTF-8 로 인코딩해서 보냈지만 브라우저가 다른 코드로 잘못 해석한 경우
+        */
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+
         PrintWriter out = resp.getWriter();
-        out.println("Hello ~~~");
+        out.println("안녕 ~~~");
     }
+
 }
